4636fc514b0ebf7b16e82cc3eb7de6b69431cd43
Imad Mouhtassem <imad@imadm.ca>
2017-08-26 14:58:48 +0000

    Fix tab spacing from 2 to 4 spaces

--- site_analytics.py
+++ site_analytics.py
@@ -9,29 +9,28 @@ import re
 
 
 def get_log_lines(path):
-  """Return a list of regex matched log lines from the passed nginx access log path"""
-  lines = []
-  with open(path) as f:
-    r = re.compile("""(?P<remote>[^ ]*) (?P<host>[^ ]*) (?P<user>[^ ]*) \[(?P<time>[^\]]*)\] "(?P<method>\S+)(?: +(?P<path>[^\"]*) +\S*)?" (?P<code>[^ ]*) (?P<size>[^ ]*)(?: "(?P<referer>[^\"]*)" "(?P<agent>[^\"]*)")""")
-    for line in f:
-      m = r.match(line)
-      if m is not None:
-        md = m.groupdict()
-        lines.append(md)
-    
+    """Return a list of regex matched log lines from the passed nginx access log path"""
+    lines = []
+    with open(path) as f:
+        r = re.compile("""(?P<remote>[^ ]*) (?P<host>[^ ]*) (?P<user>[^ ]*) \[(?P<time>[^\]]*)\] "(?P<method>\S+)(?: +(?P<path>[^\"]*) +\S*)?" (?P<code>[^ ]*) (?P<size>[^ ]*)(?: "(?P<referer>[^\"]*)" "(?P<agent>[^\"]*)")""")
+        for line in f:
+            m = r.match(line)
+            if m is not None:
+                md = m.groupdict()
+                lines.append(md)
+                
     return lines
 
 def get_ip_address_city(ip_address):
-  pass
+    pass
 
 def is_bot(useragent):
-  pass
+    pass
 
 def summarize(data):
-  pass
+    pass
 
 if __name__ == "__main__":
-  access_file_path = "/var/log/nginx/imadm.ca.access.log"
-  
-  access_log = get_log_lines(access_file_path)
+    access_file_path = "/var/log/nginx/imadm.ca.access.log"
+    access_log = get_log_lines(access_file_path)
 
